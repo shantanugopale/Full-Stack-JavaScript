75dac83d91c9b068e09039e08ab1f850b2151574
shantanugopale <gopaleshantanu@gmail.com>
2024-10-18 13:28:47 +0530

    chnages added

---
+++ Full Stack JavaScript/JavaScript/Lecture_4-Arrays_Methods/script.js
@@ -0,0 +1,9 @@
+
+// Arrays ->> Stores the Collection of values by a Single Variable.
+
+let names = ['shantanu','sushant','shivam','aniket','pooja','anurag'];
+
+console.log(names);
+
+console.log(names.length);
+
