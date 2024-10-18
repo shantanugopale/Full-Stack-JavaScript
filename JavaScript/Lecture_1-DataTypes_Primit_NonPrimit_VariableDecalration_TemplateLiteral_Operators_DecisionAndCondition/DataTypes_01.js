75e972c530d968840f390f9a1989ea3c75e2d462
shantanugopale <gopaleshantanu@gmail.com>
2024-10-18 19:58:02 +0530

    Code Base Added

---
+++ JavaScript/Lecture_1-DataTypes_Primit_NonPrimit_VariableDecalration_TemplateLiteral_Operators_DecisionAndCondition/DataTypes_01.js
@@ -0,0 +1,58 @@
+// *******************Primitive DataTypes - number, String, Boolean, (null, underfined)-> this two are empty values.*******************
+
+// 1> number
+let num1 = 2;
+let num2 = 3.8;
+
+console.log(num1);
+console.log(num2);
+// ----------------------------------------------------------------------------
+
+// 2> String --> Any Quotes used.
+let s1 = "Shantanu";
+let s2 = 'Gopale';
+let s3 = `Shantanu Gopale`
+console.log(s1);
+console.log(s2);
+console.log(s3);
+// ----------------------------------------------------------------------------
+
+// 3> Boolean --> George Boole is known as Father of Boolean Algrebra.
+let b1 = true;
+let b2= false;
+
+console.log(b1);
+console.log(b2);
+// ----------------------------------------------------------------------------
+
+// 4> null
+let e1 = null;
+console.log(e1);
+// -----------------------------
+// 5> undefined
+let e2;
+console.log(e2);
+
+// ----------------------------------------------------------------------------
+
+// *******************Non-Primitive DataTypes - Array, Objects --> More than one Value******************* 
+
+// 1> Array --> Any type can be Added.
+let arr = [3,"Shantanu", true, 9.7, null];
+// index - At what positions values are kept
+console.log(arr);
+
+// ----------------------------------------------------------------------------
+
+// 2> Objects --> {key:value} pair
+const name = {
+    // key:value;
+    fname:'shantanu',
+    lname: 'Gopale',
+    info: {
+        mail: 'gopaleshantanu@gmail.com',
+        address: 'Chakan, Pune, MahaRashtra',
+        MobNo: 7350096668
+    }
+};
+console.log(name);
